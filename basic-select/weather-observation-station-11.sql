119f0ff72d47c7463e8f7afca52f611ac2a646db
Rifa <145076282+rifa8@users.noreply.github.com>
2024-02-22 12:15:41 +0700

    Create weather-observation-station-11.sql

---
+++ basic-select/weather-observation-station-11.sql
@@ -0,0 +1,15 @@
+-- DB2, MySQL, Oracle
+select
+    distinct city
+from station
+where (upper(substr(city, 1, 1)) not in ('A', 'E', 'I', 'O', 'U')
+    or lower(substr(city, 1, 1)) not in ('a', 'e', 'i', 'o', 'u'))
+or (upper(substr(city, length(city), 1)) not in ('A', 'E', 'I', 'O', 'U')
+     or lower(substr(city, length(city), 1)) not in ('a', 'e', 'i', 'o', 'u'));
+
+-- MySQL, MS SQL Server
+select
+    distinct city
+from station
+where left(city, 1) not in ('a', 'e', 'i', 'o', 'u')
+    or right(city, 1) not in ('a', 'e', 'i', 'o', 'u');
