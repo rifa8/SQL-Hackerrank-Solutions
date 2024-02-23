e4ec86bfacfb31cd4d2e1d5f56d2b35ad72b023e
Rifa <145076282+rifa8@users.noreply.github.com>
2024-02-24 00:17:58 +0700

    Update average-population.sql

--- aggregation/average-population.sql
+++ aggregation/average-population.sql
@@ -1,9 +1,4 @@
--- DB2, MySQL, Oracle
-select
-    round(avg(population)) avg_population
-from city;
-
 -- DB2, MySQL, Oracle, MS SQL Server
 select
-    round(avg(population), 0) avg_population
+    floor(avg(population)) avg_population
 from city;
