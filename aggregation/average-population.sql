5e5cd83a4087be3e431a689e284e331e3a7dd39e
Rifa <145076282+rifa8@users.noreply.github.com>
2024-02-23 23:17:15 +0700

    Create average-population.sql

---
+++ aggregation/average-population.sql
@@ -0,0 +1,9 @@
+-- DB2, MySQL, Oracle
+select
+    round(avg(population)) avg_population
+from city;
+
+-- DB2, MySQL, Oracle, MS SQL Server
+select
+    round(avg(population), 0) avg_population
+from city;
