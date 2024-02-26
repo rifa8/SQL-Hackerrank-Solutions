7a1ab2dc8bf9c34436c2a6891300dcb4cae1b3b9
Rifa <145076282+rifa8@users.noreply.github.com>
2024-02-26 22:46:50 +0700

    Create placements.sql

---
+++ advanced-join/placements.sql
@@ -0,0 +1,12 @@
+-- DB2, MySQL, Oracle, MS SQL Server
+select
+    name
+from students s
+join friends f
+    on s.id = f.id
+join packages p1
+    on s.id = p1.id
+join packages p2
+    on f.friend_id = p2.id
+where p1.salary < p2.salary
+order by p2.salary;
