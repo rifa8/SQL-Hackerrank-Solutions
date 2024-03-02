06d3ea9c03e8d43633c0e26cc6f641fc20fa0bec
Rifa <145076282+rifa8@users.noreply.github.com>
2024-03-02 22:41:52 +0700

    Create draw-the-triangle-1.sql

---
+++ alternative-queries/draw-the-triangle-1.sql
@@ -0,0 +1,31 @@
+-- MySQL
+with recursive cte as (
+    select 20 as stars
+    union all
+    select stars - 1
+    from cte
+    where stars > 1
+)
+select repeat('* ', stars) pattern
+from cte;
+
+-- Oracle
+SET SERVEROUTPUT ON;
+BEGIN
+    FOR I IN REVERSE 1..20 LOOP
+        FOR J IN 1..I LOOP
+            DBMS_OUTPUT.PUT('* ');
+        END LOOP;
+        DBMS_OUTPUT.PUT_LINE('');
+    END LOOP;
+END;
+/
+
+-- MS SQL Server
+DECLARE @r INT;
+SET @r = 20;
+WHILE @r > 0
+BEGIN
+    PRINT REPLICATE('* ', @r);
+    SET @r = @r - 1;
+END;
